c2a599ad55253b5241a09de81ffdc607d119dee0
st3nzel <166014114+st3nzel@users.noreply.github.com>
2025-10-11 21:46:21 +0200

    Refactor sidebar.php menu item for Project Policies

--- Template/project/sidebar.php
+++ Template/project/sidebar.php
@@ -1,3 +1,5 @@
-<li <?= $this->app->checkMenuSelection('ProjectPolicyController', 'index') ? 'class="active"' : '' ?>>
-    <?= $this->url->icon('gavel', t('Project Policies'), 'ProjectPolicyController', 'index', array('plugin' => 'TaskLinkPolicies', 'project_id' => $project['id'])) ?>
+<li <?= $this->app->checkMenuSelection('ProjectPolicyController','index') ? 'class="active"' : '' ?>>
+  <?= $this->url->icon('gavel', t('Project Policies'),
+        'ProjectPolicyController', 'index',
+        ['project_id' => $project['id'], 'plugin' => 'taskLinkPolicies']) ?>
 </li>
