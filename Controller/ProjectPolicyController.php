a30c92f340bd79ca083176f735dd558d5ef18e46
st3nzel <166014114+st3nzel@users.noreply.github.com>
2025-10-11 21:33:16 +0200

    Enhance project policies page with metadata settings
    Added project metadata settings to the response for the project policies page.

--- Controller/ProjectPolicyController.php
+++ Controller/ProjectPolicyController.php
@@ -9,16 +9,29 @@ class ProjectPolicyController extends BaseController
     public function index()
     {
         $project = $this->getProject();
-        $this->logger->debug('TLP: render settings page for project '.$project['id']);
-        return $this->response->html(
-            $this->helper->layout->project(
-                'taskLinkPolicies:project/policies',
-                array(
-                    'project' => $project,
-                )
-            )
+    
+        // Alle Defaults hier lesen (nicht im Template!)
+        $settings = [
+            'tlp_enforce_blocker_move_out_backlog' => (int) $this->projectMetadataModel->get($project['id'], 'tlp_enforce_blocker_move_out_backlog', 1),
+            'tlp_enforce_blocker_close'            => (int) $this->projectMetadataModel->get($project['id'], 'tlp_enforce_blocker_close', 1),
+            'tlp_parent_requires_children_closed'  => (int) $this->projectMetadataModel->get($project['id'], 'tlp_parent_requires_children_closed', 1),
+            'tlp_milestone_requires_targets_closed'=> (int) $this->projectMetadataModel->get($project['id'], 'tlp_milestone_requires_targets_closed', 1),
+            'tlp_fix_requires_fix_task_closed'     => (int) $this->projectMetadataModel->get($project['id'], 'tlp_fix_requires_fix_task_closed', 1),
+            'tlp_duplicates_policy'                => (string) $this->projectMetadataModel->get($project['id'], 'tlp_duplicates_policy', 'allow'),
+            'tlp_backlog_column_mode'              => (string) $this->projectMetadataModel->get($project['id'], 'tlp_backlog_column_mode', 'first'),
+            'tlp_backlog_column_id'                => (int) $this->projectMetadataModel->get($project['id'], 'tlp_backlog_column_id', 0),
+            'tlp_admin_can_override'               => (int) $this->projectMetadataModel->get($project['id'], 'tlp_admin_can_override', 0),
+        ];
+    
+        $this->response->html(
+            $this->helper->layout->project('taskLinkPolicies:project/policies', [
+                'project'  => $project,
+                'settings' => $settings,
+                'title'    => t('Project Policies'),
+            ])
         );
     }
+
 
     public function save()
     {
