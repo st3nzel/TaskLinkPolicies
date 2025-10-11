74893a06e2a1eefa50c788b00bb3f6f130710825
st3nzel <166014114+st3nzel@users.noreply.github.com>
2025-10-11 23:20:12 +0200

    Fix plugin name case in sidebar.php

--- Template/project/sidebar.php
+++ Template/project/sidebar.php
@@ -1,5 +1,5 @@
 <li <?= $this->app->checkMenuSelection('ProjectPolicyController','index') ? 'class="active"' : '' ?>>
   <?= $this->url->icon('gavel', t('Project Policies'),
         'ProjectPolicyController', 'index',
-        ['project_id' => $project['id'], 'plugin' => 'taskLinkPolicies']) ?>
+        ['project_id' => $project['id'], 'plugin' => 'TaskLinkPolicies']) ?>
 </li>
